17d94564329e74fc3ee855855a9bcc4b9506205b
j.bogg <jonathan.bogg@gmail.com>
2020-02-27 15:36:19 +0100

    test

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <h1>
+                Footer
+            </h1>
+        );
+    }
+}
+
+export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import Header from "./components/Header"
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <>
+    <Hello compiler="TypeScript" framework="React" />
+    <Header/></>,
     document.getElementById("example")
 );
