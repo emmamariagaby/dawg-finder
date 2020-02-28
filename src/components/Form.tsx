50d7ea511de8f61175ad0e7861c74ae1d3e772f9
emmamariagaby <emmamariagaby@gmail.com>
2020-02-28 12:01:01 +0100

    create class form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,34 @@
 import * as React from "react";
 
-class Form extends React.Component {
-    render() {
-        return (
-          <h1> Form </h1>
-        
-        );
-    }
+import Button from "./Button";
+
+export interface FormProps {
+  question: number
+}
+
+class Form extends React.Component<FormProps, {}> {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="questionOne">Val 1</label>
+            <input type="radio" name="questionOne" />
+            <label htmlFor="B">Val 2</label>
+            <input type="radio" name="B" />
+            <label htmlFor="C">Val 3</label>
+            <input type="radio" name="C" />
+          </form>
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
+      </div>
+    );
+  }
+
+  handleSubmit() {
+    // spara value
+  }
 }
 
 export default Form;
