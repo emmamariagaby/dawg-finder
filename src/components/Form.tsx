1115f3814b1889842bcebf83c96db75582487302
emmamariagaby <emmamariagaby@gmail.com>
2020-03-03 17:02:01 +0100

    working on function to save radio value

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,56 +1,57 @@
 import * as React from "react";
 
+
+
 class Form extends React.Component {
    // togglePrev: ((event: MouseEvent<HTMLButtonElement, MouseEvent>) => void) | undefined;
     //toggleNext: ((event: MouseEvent<HTMLButtonElement, MouseEvent>) => void) | undefined;
-
+    
+    
   render() {
     return (
       <div className="wrapper">
         <div className="form-wrapper">
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="A">Val A</label>
-            <input type="radio" name="test" value="value 1"/>
-            <label htmlFor="B">Val B</label>
-            <input type="radio" name="test" value="value 2"/>
-            <label htmlFor="C">Val C</label>
-            <input type="radio" name="test" value="value 3"/> 
-          </form>
+   
           {/* <button className="toggle-prev" onClick={this.togglePrev}>Previous</button> */}
           {/* <button className="toggle-next" onClick={this.toggleNext}>Next</button> */}
         </div>
       </div>
     );
   }
+ 
 
-   data = [
-    {
-     
-    }
-];
+    displayRadioValue(id: string){ 
+      const radios: HTMLInputElement = document.getElementById("active") as HTMLInputElement; // list of radio buttons 
+      var value = localStorage.getElementById("active").value; // local storage value
+      
+      for (var i = 0; i < radios.length; i++) {
+        if (radios[i].value == value){
+            radios[i].checked = true; // marking the required radio as checked
+            
+          }
+        
+      }
+                       
+        } 
+    } 
+  
 
-  handleSaveValue() {
-    var answ1 = document.querySelector('input[name="group"]:checked').value;
+    
 
-    if (answ1.value == 1){
-          answ1.setAttribute("checked","checked");
-          answ1.checked = true;
-    }
-    }
 
-  handleSubmit() {
-    // hantera submit all values 
-  }
+//   handleSubmit() {
+//     // hantera submit all values 
+//   }
 
-  handleTogglePrev() {
-      console.log("something worked toggle prev");
-      // gå till nästa fråga
-  }
+//   handleTogglePrev() {
+//       console.log("something worked toggle prev");
+//       // gå till nästa fråga
+//   }
 
-  handleToggleNext() {
-      console.log("something worked toggle next");
-      // gå tillbaka till fråga
-  }
-}
+//   handleToggleNext() {
+//       console.log("something worked toggle next");
+//       // gå tillbaka till fråga
+//   }
 
+  
 export default Form;
