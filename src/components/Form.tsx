a1ebcc5ed0567880a2998a78f4d5627d44e164c8
j.bogg <jonathan.bogg@gmail.com>
2020-03-03 18:12:50 +0100

    form test

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,35 +1,64 @@
 import * as React from "react";
 
+export interface FormState {
+  chosenValue: string
+}
 
+class Form extends React.Component<{}, FormState> {
 
-class Form extends React.Component {    
-    
+  constructor(props?: any) {
+    super(props);
+    this.state = { 
+      chosenValue: "" 
+    }
+  }
+
   render() {
     return (
-      <div className="wrapper">
-        <div className="form-wrapper">
-  
-        </div>
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <h1> Form </h1>
+        <ul>
+          <h2>How active are you?</h2>
+            <input
+              type="radio"
+              name="active"
+              checked={this.state.chosenValue === "Very active"}
+              onChange={this.onRadioChange}
+              value="Very active" />
+              <label htmlFor="Very active">Very active</label>
+          
+            <input
+              type="radio"
+              name="active"
+              checked={this.state.chosenValue === "Not very active"}
+              onChange={this.onRadioChange}
+              value="Not very active" />
+               <label htmlFor="Not very active">Not very active</label>
+
+              <input
+              type="radio"
+              name="active"
+              checked={this.state.chosenValue === "I'm an atletic"}
+              onChange={this.onRadioChange}
+              value="I'm an atletic" />
+               <label htmlFor="I'm an atletic">I'm an atletic</label>
+          
+          <button type="submit">Submit</button>
+        </ul>
+      </form>
     );
   }
- 
 
-    displayRadioValue(){ 
-      const radios: HTMLInputElement = document.getElementById("active") as HTMLInputElement; // list of radio buttons 
-      var value = localStorage.getElementById("active").value; // local storage value
-      
-      for (var i = 0; i < radios.length; i++) {
-        if (radios[i].value == value){
-            radios[i].checked = true; // marking the required radio as checked
-            
-          }
-        
-      }
-                       
-        } 
-    } 
-  
+  onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
 
-  
+  onRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      chosenValue: e.target.value
+    })
+  }
+}
+
 export default Form;
