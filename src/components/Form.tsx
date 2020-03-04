10230ad0831b9ac2688d6dcf0a90ead11005c00f
emmamariagaby <emmamariagaby@gmail.com>
2020-03-04 17:30:33 +0100

    create local storage

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 
 export interface FormState {
-  chosenValue: string
+  firstValue: string
+  secondValue: string
 }
 
 class Form extends React.Component<{}, FormState> {
@@ -9,6 +10,8 @@ class Form extends React.Component<{}, FormState> {
   constructor(props?: any) {
     super(props);
     this.state = { 
+      firstValue: "",
+      secondValue: ""
     }
   }
 
@@ -17,30 +20,60 @@ class Form extends React.Component<{}, FormState> {
       <form onSubmit={this.onSubmit}>
         <h1> Form </h1>
         <ul>
+
+          <section className="questionOne">
           <h2>How active are you?</h2>
             <input
               type="radio"
               name="active"
-              checked={this.state.chosenValue === "Very active"}
-              onChange={this.onRadioChange}
+              checked={this.state.firstValue === "Very active"}
+              onChange={this.onRadioChangeFirst}
               value="Very active" />
               <label htmlFor="Very active">Very active </label>
           
             <input
               type="radio"
               name="active"
-              checked={this.state.chosenValue === "Not very active"}
-              onChange={this.onRadioChange}
+              checked={this.state.firstValue === "Not very active"}
+              onChange={this.onRadioChangeFirst}
               value="Not very active" />
                <label htmlFor="Not very active">Not very active </label>
 
               <input
               type="radio"
               name="active"
-              checked={this.state.chosenValue === "I'm an atletic"}
-              onChange={this.onRadioChange}
+              checked={this.state.firstValue === "I'm an atletic"}
+              onChange={this.onRadioChangeFirst}
               value="I'm an atletic"/>
                <label htmlFor="I'm an atletic">I'm an atletic </label>
+               </section>
+
+              <section className="questionTwo">
+               <h2>Do you have any special interest?</h2>
+              <input
+              type="radio"
+              name="interest"
+              checked={this.state.secondValue === "I'm interested in hunting, search or rescue training"}
+              onChange={this.onRadioChangeTwo}
+              value="I'm interested in hunting, search or rescue training" />
+              <label htmlFor="I'm interested in hunting, search or rescue training">I'm interested in hunting, search or rescue training </label>
+          
+            <input
+              type="radio"
+              name="interest"
+              checked={this.state.secondValue === "I'm interested in herding sheep and living the farm life"}
+              onChange={this.onRadioChangeTwo}
+              value="I'm interested in herding sheep and living the farm life" />
+               <label htmlFor="I'm interested in herding sheep and living the farm life">I'm interested in herding sheep and living the farm life </label>
+
+              <input
+              type="radio"
+              name="active"
+              checked={this.state.secondValue === "I have no special interest"}
+              onChange={this.onRadioChangeTwo}
+              value="I have no special interest"/>
+               <label htmlFor="I have no special interest">I have no special interest </label>
+               </section>
           
           <li>
             <label htmlFor="Yes">Yes</label>
@@ -48,7 +81,7 @@ class Form extends React.Component<{}, FormState> {
               type="radio"
               name="active"
               checked={this.state.chosenValue === "Yes"}
-              onChange={this.onRadioChange}
+              onChange={this.onRadioChangeOne}
               value="Yes" />
           </li>
           <li>
@@ -70,14 +103,22 @@ class Form extends React.Component<{}, FormState> {
   onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     console.log(this.state)
-    const { chosenValue } = this.state;
-    localStorage.setItem('chosenValue', chosenValue);
+    const { firstValue } = this.state;
+    localStorage.setItem('firstValue', firstValue);
+    const { secondValue } = this.state;
+    localStorage.setItem('secondValue', secondValue);
   }
 
-  onRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  onRadioChangeFirst = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({
-      chosenValue: e.target.value
+      firstValue: e.target.value,
     })
+  }
+      onRadioChangeTwo = (e: React.ChangeEvent<HTMLInputElement>) => {
+      this.setState({
+        secondValue: e.target.value
+      })
+
   }
 }
   // togglePrev: ((event: MouseEvent<HTMLButtonElement, MouseEvent>) => void) | undefined;
