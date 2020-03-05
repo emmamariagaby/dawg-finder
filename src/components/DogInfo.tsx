d9fe6ea973b3af63b2d9af46575bff219fb65d71
j.bogg <jonathan.bogg@gmail.com>
2020-03-05 17:52:36 +0100

    setup DogInfo and shared interface props

--- src/components/DogInfo.tsx
+++ src/components/DogInfo.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 
-class DogInfo extends React.Component {
-    render() {
-        return (
-          <h1> DogInfo </h1>
-        
-        );
-    }
+import DogImage from "./DogImage";
+import DogText from "./DogText";
+
+export interface DogInfoProps {
+  breed?: string
+  dogType: string
+}
+
+class DogInfo extends React.Component<DogInfoProps, {}> {
+  render() {
+    return (
+      <>
+        <DogImage dogType={this.props.dogType} />
+        <DogText dogType={this.props.dogType} />
+      </>
+    );
+  }
 }
 
 export default DogInfo;
