828abe4ac39c6d1cdd784133b8022352cd0b35bb
j.bogg <jonathan.bogg@gmail.com>
2020-03-05 20:36:31 +0100

    dogtext basic setup

--- src/components/DogText.tsx
+++ src/components/DogText.tsx
@@ -1,12 +1,44 @@
 import * as React from "react";
 
-class DogText extends React.Component {
-    render() {
+import { DogInfoProps } from "./DogInfo";
+
+export interface DogTextState {
+  textSource: string
+}
+
+export interface DogTextProps {
+  [key: string]: string
+}
+
+class DogText extends React.Component<DogTextProps, DogTextState> {
+  render() {
+    return (
+      <>
+        <h3>{this.props.dogType}</h3>
+        <p>{this.state.textSource}</p>
+      </>
+    );
+  }
+
+  fetchDogText() {
+    fetch("./src/components/dogtext.json")
+      .then(response => response.json())
+      .then(data => {
         return (
-          <h1> DogText </h1>
-        
-        );
-    }
+          this.setState({
+            textSource: this.getDogText(data) as string
+          })
+        )
+      })
+      .catch(error => console.error(error))
+  }
+
+  getDogText(data: DogTextProps) {
+      for (let i = 0; i < Object.keys(data).length; i++) {
+        return "hej"
+      }
+      return "då"
+  }
 }
 
 export default DogText;
