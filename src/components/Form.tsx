9d539856a111a7f1ae10e7d89f085da869bd0ec8
AVLlarsson <annlar2@hotmail.com>
2020-03-06 16:31:55 +0100

    question increment/decrement

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 export interface FormState {
   firstValue: string
   secondValue: string
+  questionNumber: number
 }
 
 class Form extends React.Component<{}, FormState> {
@@ -10,10 +11,13 @@ class Form extends React.Component<{}, FormState> {
   constructor(props?: any) {
     super(props);
     this.state = { 
+      questionNumber: 1,
       firstValue: "",
       secondValue: ""
+
     }
    this.questionParse = this.questionParse.bind(this);
+   this.previousQuestion = this.previousQuestion.bind(this);
   }
 
 questionParse(q: number) {
@@ -21,7 +25,7 @@ questionParse(q: number) {
   return (
         <div style={{color: 'green'}} className="formQuestions"> 
         Fråga 1
-<section className="questionOne">
+        <section className="questionOne">
           <h2>How active are you?</h2>
             <input
               type="radio"
@@ -47,32 +51,15 @@ questionParse(q: number) {
               value="I'm an athletic"/>
                <label htmlFor="I'm an athletic">I'm an athletic </label>
                </section>
-
         </div>
         )
         }
 
         if (q == 2) {
   return (
-        <div style={{color: 'green'}} className="formQuestions"> 
+        <div style={{color: 'blue'}} className="formQuestions"> 
         Fråga 2
-        </div>
-        )
-        } 
-        return (null);
-}
-
-
-  render() {
-    return (
-      <form onSubmit={this.onSubmit}>
-        <h1> Form </h1>
-        <ul>
-
-     {this.questionParse(1)}
-     
-          
-              <section className="questionTwo">
+ <section className="questionTwo">
                <h2>Do you have any special interest?</h2>
               <input
               type="radio"
@@ -98,8 +85,59 @@ questionParse(q: number) {
               value="I have no special interest"/>
                <label htmlFor="I have no special interest">I have no special interest </label>
                </section>
+        </div>
+        )
+        } 
+       if (q == 3) {
+  return (
+  
 
-          <button type="button">Previous</button>
+       <div style={{color: 'red'}} className="formQuestions"> 
+        Fråga 3
+<section className="questionThree">
+          <h2>Do you like dawgs?</h2>
+            <input
+              type="radio"
+              name="likes"
+              checked={this.state.firstValue === "Very mucho!"}
+              onChange={this.onRadioChangeFirst}
+              value="Very mucho!" />
+              <label htmlFor="Very mucho!">Very mucho! </label>
+          
+            <input
+              type="radio"
+              name="likes"
+              checked={this.state.firstValue === "Not at all"}
+              onChange={this.onRadioChangeFirst}
+              value="Not at all" />
+               <label htmlFor="Not at all">Not at all </label>
+
+              <input
+              type="radio"
+              name="likes"
+              checked={this.state.firstValue === "Yaaas ofc!"}
+              onChange={this.onRadioChangeFirst}
+              value="Yaaas ofc!"/>
+               <label htmlFor="Yaaas ofc!">Yaaas ofc! </label>
+               </section>
+        </div>
+   ) }  
+   return (null);
+        }  
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1> Form </h1>
+        <ul>
+
+     {this.questionParse(this.state.questionNumber)}
+     
+
+        <button type="button" onClick={() => this.previousQuestion()}>
+             Previous 
+          </button>
+      
           <button type="submit">Next</button>
         
         </ul>
@@ -107,14 +145,32 @@ questionParse(q: number) {
     );
   }
 
+  previousQuestion(){
+    if(this.state.questionNumber == 1 ){
+      alert('Nu blev det fel!')
+    } else {
+    this.setState({
+      questionNumber: this.state.questionNumber - 1
+  })
+    }
+}
+
   onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    if(this.state.questionNumber == 3 ){
+      alert('Visa resultat!')
+    } else {
+  e.preventDefault()
     console.log(this.state)
     const { firstValue } = this.state;
     localStorage.setItem('firstValue', firstValue);
     const { secondValue } = this.state;
     localStorage.setItem('secondValue', secondValue);
+    this.setState({
+      questionNumber: this.state.questionNumber + 1
+    })
+    }
   }
+
 
   onRadioChangeFirst = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({
