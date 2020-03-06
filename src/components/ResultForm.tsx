3bd3df080a273f934341e52c76aafabb364a3286
emmamariagaby <emmamariagaby@gmail.com>
2020-03-06 18:40:38 +0100

    setup ResultForm

---
+++ src/components/ResultForm.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class ResultForm extends React.Component {
+    render() {
+        return (
+          <h1> ResultForm </h1>
+        
+        );
+    }
+}
+
+
+export default ResultForm;
