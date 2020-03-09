9b9b64946010ad48153fdf0eb4fdfe4c0c0abbb6
AVLlarsson <annlar2@hotmail.com>
2020-03-09 11:15:35 +0100

    First router test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import  * as React from "react";
 import * as ReactDOM from "react-dom";
-// import { BrowserRouter as Router, BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, BrowserRouter, Switch, Route, Link } from "react-router-dom";
 // import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -8,72 +8,104 @@ import Footer from "./components/Footer";
 import ResultRouter from "./ResultRouter";
 import Option from "./components/Option";
 
-// import Form from "./components/Form";
+import Form from "./components/Form";
 import Button from "./components/Button";
 import Container from "./components/Container";
-// import ResultsContainer from "./components/ResultsContainer";
-// import {FormRender} from "./components/Form";
+import ResultsContainer from "./components/ResultsContainer";
+import FormRender from "./components/Form";
 
-// const RouteApp: React.FC = () => {
-//   return (
-//     <BrowserRouter>
-//       <Route path="/" />
-//       <Route path="/form" component={FormRender} />
-//      </BrowserRouter>
-//   );
-// };
+const RouteApp: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" />
+      <Route path="/form" component={FormRender} />
+     </BrowserRouter>
+  );
+};
 
 
 
-// export default function Index() {
-//     return(
-//         <Router>
-//       <div>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/components/Form">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/components/ResultsContanier">Users</Link>
-//             </li>
-//           </ul>
-//         </nav>
+export default function Index() {
+    return(
+        <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/components/Form">About</Link>
+            </li>
+            <li>
+              <Link to="/components/ResultsContanier">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-//         <Switch> 
-//             <Route path="/components/Form">
-//                 <Form />
-//             </Route>
-//             <Route path="/components/ResultsContanier">
-//                 <ResultsContainer result="sällskap" />
-//             </Route>
-//             <Route path="/">
-//                 <Form />
-//             </Route>
-//              </Switch>
-//       </div>
-//     </Router>
-//     )
-// }
+        <Switch> 
+            <Route path="/components/Form">
+                <Form />
+            </Route>
+            <Route path="/components/ResultsContanier">
+                <ResultsContainer result="sällskap" />
+            </Route>
+            <Route path="/">
+                <Form />
+            </Route>
+             </Switch>
+      </div>
+    </Router>
+    )
+}
 
 
 ReactDOM.render(
-    <>
-        <Header />
-        <ResultRouter />
-        {/* <BrowserRouter>
-            <Index />
-        </BrowserRouter> */}
-        <Option />
-        {/* <Form /> */}
-        {/* {this.RouteApp} */}
-        <Button />
-        <Container />
-        {/* <ResultsContainer result="sällskap" /> */}
-        <Footer />
-    </>,
+
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/components/Form">About</Link>
+            </li>
+            <li>
+              <Link to="/components/ResultsContanier">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch> 
+            <Route path="/components/Form">
+                <Form />
+            </Route>
+            <Route path="/components/ResultsContanier">
+                <ResultsContainer result="sällskap" />
+            </Route>
+            <Route path="/">
+                <Form />
+            </Route>
+             </Switch>
+      </div>
+    </Router>,
+
+    // <>
+    //     <Header />
+    //     <Index />
+    //     {/* <ResultRouter /> */}
+    //     {/* <BrowserRouter>
+    //         <Index />
+    //     </BrowserRouter> */}
+    //     <Option />
+    //     {/* <Form /> */}
+    //     {/* {this.RouteApp} */}
+    //     <Button />
+    //     <Container />
+    //     {/* <ResultsContainer result="sällskap" /> */}
+    //     <Footer />
+    // </>,
     document.getElementById("root")
 );
