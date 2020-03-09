18d0dcf6cf7252586f9474daf8fec4f54d0512c9
AVLlarsson <annlar2@hotmail.com>
2020-03-09 11:39:37 +0100

    Made a functional router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,14 +16,49 @@ import Container from "./components/Container";
 import ResultsContainer from "./components/ResultsContainer";
 
 ReactDOM.render(
-    <>
-        <Header />
-        <Option />
-        <Form />
-        <Button />
-        <Container />
-        <ResultsContainer result="sällskap" />
-        <Footer />
-    </>,
+ 
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/form">Home</Link>
+            </li>
+            <li>
+              <Link to="/resultContainer">About</Link>
+            </li>
+            <li>
+              <Link to="/footer">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/form">
+            <Form />
+          </Route>
+          <Route path="/resultContainer">
+            <ResultsContainer result="sällskap" />
+          </Route>
+          <Route path="/footer">
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>,
+  
+
+
+    // <>
+    //     <Header />
+    //     <Option />
+    //     <Form />
+    //     <Button />
+    //     <Container />
+    //     <ResultsContainer result="sällskap" />
+    //     <Footer />
+    // </>,
     document.getElementById("root")
 );
