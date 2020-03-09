f7ce204d2e50eb738305ca1f64f499a4a41c4a46
AVLlarsson <annlar2@hotmail.com>
2020-03-09 11:54:12 +0100

    merged successfully with master

--- src/index.tsx
+++ src/index.tsx
@@ -62,3 +62,21 @@ ReactDOM.render(
     // </>,
     document.getElementById("root")
 );
+
+
+
+// import * as React from "react";
+// import { BrowserRouter, Route } from "react-router-dom";
+// import Form from "./components/Form";
+// import ResultsContanier from "./components/ResultsContainer";
+
+// const ResultRouter: React.FC = () =>{
+//     return (
+//         <BrowserRouter>
+//             <Route path="/" component={Form} />
+//             <Route path="/a" component={ResultsContanier} />
+//         </BrowserRouter>   
+//     );
+// };
+
+// export default ResultRouter;
