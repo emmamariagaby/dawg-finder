ca16e7c14421000de3af3f06ea40cf536fac886f
AVLlarsson <annlar2@hotmail.com>
2020-03-09 14:03:13 +0100

    Basic setup done

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from "react-dom";
 // import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import Home from "./components/screens/Home";
 import Footer from "./components/Footer";
 import ResultRouter from "./ResultRouter";
 import Option from "./components/Option";
@@ -14,15 +15,40 @@ import Container from "./components/Container";
 // import ResultsContainer from "./components/ResultsContainer";
 // import {FormRender} from "./components/Form";
 
-// const RouteApp: React.FC = () => {
-//   return (
-//     <BrowserRouter>
-//       <Route path="/" />
-//       <Route path="/form" component={FormRender} />
-//      </BrowserRouter>
-//   );
-// };
+ReactDOM.render(
+ 
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/resultContainer">About</Link>
+            </li>
+            <li>
+              <Link to="/footer">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/resultContainer">
+            <ResultsContainer result="sällskap" />
+          </Route>
+          <Route path="/footer">
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>,
+  
 
 
 // export default function Index() {
