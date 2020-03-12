981eac77cd8116ca8455788288cc7b18a14c6bb9
j.bogg <jonathan.bogg@gmail.com>
2020-03-12 16:52:41 +0100

    make Container.tsx responsive, add main css to body and Form

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,56 @@
 import * as React from "react";
 
-class Container extends React.Component {
-    render() {
-        return (
-          <div className="container">
-            {this.props.children}
-          </div>
-        );
-    }
+import "../styles/main.css"
+
+export interface ContainerState {
+	windowWidth: number
+	windowHeight: number
+}
+
+class Container extends React.Component<{}, ContainerState> {
+	constructor(props) {
+		super(props);
+		this.state = {
+			windowWidth: 0,
+			windowHeight: 0,
+		}
+
+		this.updateScreenSize = this.updateScreenSize.bind(this);
+	}
+
+	render() {
+		return (
+			<div className={"container " + this.getClassName()}>
+				{this.props.children}
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		this.updateScreenSize();
+		window.addEventListener("resize", this.updateScreenSize);
+
+	}
+
+	updateScreenSize() {
+		this.setState({
+			windowWidth: (window.innerWidth != undefined) ? window.innerWidth : 0,
+			windowHeight: (window.innerHeight != undefined) ? window.innerHeight : 0,
+		})
+	}
+
+	getClassName() {
+		if (this.state.windowWidth < 640) {
+			return "minScreen";
+		}
+		else {
+			return "medScreen";
+		}
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.updateScreenSize);
+	}
 }
 
 export default Container;
