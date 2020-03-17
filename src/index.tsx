8dd07cd5c43656589002df494da03d4a9a902e92
j.bogg <jonathan.bogg@gmail.com>
2020-03-17 16:36:16 +0100

    clean up index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -21,8 +21,6 @@ ReactDOM.render(
         </NavigationMenu>
       </nav>
 
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
       <Switch>
         <Route path="/find-your-dog">
           <FormScreen />
