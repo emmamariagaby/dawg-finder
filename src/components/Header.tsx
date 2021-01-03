159783a81b28b97242be9c1d12ff0f8849594843
Jonathan Bogg <jonathan.bogg@gmail.com>
2021-01-03 14:39:19 +0100

    Update img src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ class Header extends React.Component<HeaderProps, {}> {
 		return (
 			<div className={`header 
 		  		${this.props.cssClass != undefined ? this.props.cssClass : ""}`}>
-				<img src="mockups/logo/Header-Dawg-2.png"
+				<img src="docs/mockups/logo/Header-Dawg-2.png"
 					alt="Logo with page title and illustration of a dog" />
 			</div>
 		);
